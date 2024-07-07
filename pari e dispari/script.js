bbed04cefecc0159ff14d2b4645682609837304f
andrearomeo04 <pisco004@gmail.com>
2024-07-07 23:43:16 +0200

    [UPD] added function and if inside for check of even or odd

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,3 +1,10 @@
 function andomnumber(){
-    return Math.floor(math.random() * 5 + 1)
+    return Math.floor(math.random() * 5 + 1);
+}
+
+function evenodd (sum) {
+    if (sum % 2 === 0) {
+        return 'pari'
+    }
+    return 'dispari'
 }
