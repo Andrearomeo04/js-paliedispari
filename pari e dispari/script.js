1c93aeb957212edf3f61cde36eca6ece7e144196
andrearomeo04 <pisco004@gmail.com>
2024-07-07 23:48:48 +0200

    [UPD] declared 3 variables, 2 are prompts and the third is the computer number

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,4 +1,4 @@
-function andomnumber(){
+function randomnumber(){
     return Math.floor(math.random() * 5 + 1);
 }
 
@@ -8,3 +8,10 @@ function evenodd (sum) {
     }
     return 'dispari'
 }
+
+let even_odd = prompt('scegli "pari" o "dispari"')
+
+let num = prompt('scegli un numero tra 1 e 5')
+
+let computer_num = randomnumber()
+
