c3ad4364aad665671d8630239d22970828c3c421
andrearomeo04 <pisco004@gmail.com>
2024-07-08 00:32:52 +0200

    [UPD] added if to display if the word is palindrome or not

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,6 +1,14 @@
 function check (word) {
     let reverse_word
     reverse_word = word.split('').reverse('').join('')
+
+    if (reverse_word.toLowerCase() === word.toLowerCase()) {
+        console.log('la parola è palindroma!')
+    } else {
+        console.log('la parola non è palindroma... prova con un altra!')
+    }
 }
 
 let ins_word = prompt('inserisci una parola')
+
+check(ins_word.toLowerCase())
